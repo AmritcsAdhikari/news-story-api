f8115a44be4075d477b3db14fe867108df1ad049
amradh <amritadhikari404@gmail.com>
2023-04-06 22:31:11 -0500

    Init

---
+++ news-story-application/src/test/java/com/app/story/NewsStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.story;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
